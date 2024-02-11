c4a6daf95806b96c9154eb29a4d8186397079ee9
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2024-02-11 10:52:16 +0530

    🎉 initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to chat app backend");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
